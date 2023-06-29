09d34759c5bea009f4600e50a9175760fd7ced4c
shivamadrosonics <125643910+shivamadrosonics@users.noreply.github.com>
2023-06-29 12:23:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// console.log("hello")
+var obj = {
+    name: "Shivam",
+    age: 22,
+    city: "Ranchi"
+}
+
+for(let i in obj){
+    console.log(obj[i]);
+}
+
+setTimeout(() => {
+    console.log("3 second baad aaya hoon")
+}, 3000);
+
+console.log("Hello, World!")
+
